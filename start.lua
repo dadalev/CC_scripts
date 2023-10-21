8de32461aa93c39155c5366aa06634ff184b7d10
David Rose <dadalev-9@seznam.cz>
2023-10-22 00:27:13 +0200

    first commit

---
+++ start.lua
@@ -0,0 +1,15 @@
+print("Start")
+
+print("Enter file name:")
+local fileName = read("*")
+
+local function runGit()
+    fs.delete(fileName .. ".lua")
+    shell.run("wget https://github.com/dadalev/CC_villagers/raw/main/" .. fileName .. ".lua")
+    shell.run(fileName .. ".lua")
+end
+
+runGit()
+
+print("newTest")
+print("End")
