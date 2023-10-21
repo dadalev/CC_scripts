eba5a918f2ec8f093f66adce0467752b9291ee79
David Rose <dadalev-9@seznam.cz>
2023-10-22 01:08:17 +0200

    test2

--- start.lua
+++ start.lua
@@ -1,11 +1,14 @@
 print("Start")
 
+print("Enter repository name:")
+local repoName = read("*")
+
 print("Enter file name:")
 local fileName = read("*")
 
 local function runGit()
     fs.delete(fileName .. ".lua")
-    shell.run("wget https://github.com/dadalev/CC_villagers/raw/main/" .. fileName .. ".lua")
+    shell.run("wget https://github.com/dadalev/" .. repoName .. "/raw/main/" .. fileName .. ".lua")
     shell.run(fileName .. ".lua")
 end
 
