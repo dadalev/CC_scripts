0183599cea511ed985127c1be044ae4cad6a0150
David Rose <dadalev-9@seznam.cz>
2023-10-22 02:58:15 +0200

    x

--- setup.lua
+++ setup.lua
@@ -1,1 +1,29 @@
-print("Hello World!")
+local monitors = {
+    peripheral.wrap("monitor_9"),
+    peripheral.wrap("monitor_10"),
+    peripheral.wrap("monitor_11"),
+    peripheral.wrap("monitor_12"),
+    peripheral.wrap("monitor_13"),
+    peripheral.wrap("monitor_14")
+}
+
+local function clear()
+    for _,mon in pairs( monitors ) do
+      mon.clear()
+    end
+end
+
+clear()
+
+local function write( ... )
+    for _,mon in pairs( monitors ) do
+      mon.write( ... )
+    end
+end
+
+write("test")
+
+print("Fire")
+print("Sweep")
+print("")
+print("Rip")
